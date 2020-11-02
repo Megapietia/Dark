4b9333cf3b221fae87368623fe158d62f07a0b65
Megapietia <73172090+Megapietia@users.noreply.github.com>
2020-11-02 19:17:43 +0500

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,9 +1,10 @@
-setTimeout(function(){
-	document.body.classList.add('body_visible');
-}, 200);
+// setTimeout(function(){
+// 	document.body.classList.add('body_visible');
+// }, 200);
 
 
 
 function myFunction() {
 	document.getElementById("growing").style.transform = "matrix(1.2,0,0.0,1.2,45,0)"; 
   }
+
