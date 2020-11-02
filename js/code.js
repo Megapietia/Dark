838705c47ed8d50daf0edc53e3716646db709cd5
Megapietia <73172090+Megapietia@users.noreply.github.com>
2020-11-02 23:45:07 +0500

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -4,7 +4,17 @@
 
 
 
-function myFunction() {
-	document.getElementById("growing").style.transform = "matrix(1.2,0,0.0,1.2,45,0)"; 
-  }
+// function myFunction() {
+// 	document.getElementById("growing").style.transform = "matrix(1.2,0,0.0,1.2,45,0)"; 
+//   }
+
+  // document.getElementById('zoom_btn').onclick = function() {
+	// document.getElementById('main').classList.toggle('zoom-left');
+
+  // }
+  // document.getElementById('zoom_btn2').onclick = function() {
+	// document.getElementById('main2').classList.toggle('zoom-left');
+  // }
+
+
 
