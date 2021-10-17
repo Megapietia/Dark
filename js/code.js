0c3af37ee1f8abaf433a1dca0b7a86835c1e1146
Vadim Mironov <73172090+Megapietia@users.noreply.github.com>
2021-10-17 14:47:25 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,20 @@
+// setTimeout(function(){
+// 	document.body.classList.add('body_visible');
+// }, 200);
+
+
+
+// function myFunction() {
+// 	document.getElementById("growing").style.transform = "matrix(1.2,0,0.0,1.2,45,0)"; 
+//   }
+
+  // document.getElementById('zoom_btn').onclick = function() {
+	// document.getElementById('main').classList.toggle('zoom-left');
+
+  // }
+  // document.getElementById('zoom_btn2').onclick = function() {
+	// document.getElementById('main2').classList.toggle('zoom-left');
+  // }
+
+
+
